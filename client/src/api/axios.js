aacde458b21e8d44157b500224c591645a68a368
AkashdeepSingh2002R <156530670+AkashdeepSingh2002R@users.noreply.github.com>
2025-08-15 21:40:19 -0400

    bug fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const BASE_URL =
   import.meta.env.VITE_API_URL ||
   import.meta.env.VITE_API_BASE || // if you kept older name
-  'http://localhost:5000/api';
+  'https://schoolmanagementsystem-pscb.onrender.com';
 
 const api = axios.create({ baseURL: BASE_URL });
 
